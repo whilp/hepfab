bdbace46e3fc7d5f8939d31c65ef1509e0e13cc6
Will Maier <willmaier@ml1.net>
2010-11-11 08:38:57 -0600

    Add genrange() tests.

--- tests/test_util.py
+++ tests/test_util.py
@@ -12,3 +12,24 @@ class TestDigits(BaseTest):
     
     def test_negative(self):
         self.assertEqual(digits(-11), 2)
+
+class TestGenrange(BaseTest):
+    
+    def test_simple(self):
+        gen = list(genrange("g10", 24))
+        self.assertEqual(len(gen), 24)
+        self.assertEqual(gen[0], "g10n01")
+        self.assertEqual(gen[-1], "g10n24")
+
+    def test_startstop(self):
+        gen = list(genrange("g10", 10, 24))
+        self.assertEqual(len(gen), 15)
+        self.assertEqual(gen[0], "g10n10")
+        self.assertEqual(gen[-1], "g10n24")
+
+    def test_starstopstep(self):
+        gen = list(genrange("g10", 10, 24, 2))
+        self.assertEqual(len(gen), 8)
+        self.assertEqual(gen[0], "g10n10")
+        self.assertEqual(gen[1], "g10n12")
+        self.assertEqual(gen[-1], "g10n24")
