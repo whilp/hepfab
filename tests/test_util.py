85c0fec63c62b55cc83484fe5a9e0a756e5e3dd1
Will Maier <willmaier@ml1.net>
2010-11-11 08:13:28 -0600

    Add more digits tests.

--- tests/test_util.py
+++ tests/test_util.py
@@ -4,5 +4,11 @@ from hepfab import util
 
 class TestDigits(BaseTest):
     
-    def test_foo(self):
+    def test_ten(self):
         self.assertEqual(util.digits(10), 2)
+    
+    def test_zero(self):
+        self.assertEqual(util.digits(0), 1)
+    
+    def test_negative(self):
+        self.assertEqual(util.digits(-11), 2)
